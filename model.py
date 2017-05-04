515c3fe21eaed172594c1a212a3731e345754b3f
Peter Schüllermann <Peter.schuellermann>
2017-05-04 15:18:58 +0200

    add first keras model

--- model.py
+++ model.py
@@ -44,5 +44,14 @@ Y_train = np.array(measurements)
 # todo: split data
 
 
-from keras.models import Seqential
+from keras.models import Sequential
 from keras.layers import Flatten, Dense
+
+model = Sequential()
+model.add(Flatten(input_shape=(160,320,3)))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, Y_train, validation_split=0.2, shuffle=True)
+
+model.save('model.h5')
