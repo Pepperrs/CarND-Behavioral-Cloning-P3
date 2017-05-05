3f5a3233b1a024a2f80d8e41e5f6323df78126da
Peter Schüllermann <Peter.schuellermann>
2017-05-05 13:38:36 +0200

    add nvidia pipeline

--- model.py
+++ model.py
@@ -41,6 +41,9 @@ for line in lines:
 X_train = np.array(images)
 Y_train = np.array(measurements)
 
+
+# todo: preprocessing
+
 # todo: split data
 
 
@@ -52,22 +55,42 @@ model = Sequential()
 model.add(Flatten(input_shape=(160,320,3)))
 
 # Convolution 24@31x98
+model.add(Conv2D(24, 5))
 
 # Convolution 36@14x47
+model.add(Conv2D(32, 5))
+
 
 # Convolutioin 48@5x22
+model.add(Conv2D(48, 3))
+
 
 # Convolution 64@3x20
+model.add(Conv2D(60, 3))
+
 
 # Convolution 64@1x18
+model.add(Conv2D(60, 3))
+
+
+# Flatten
+model.add(Flatten())
 
 # Fully connected 1164 neurons
+model.add(Dense(1164))
+
 
 # Fully connected 100 neurons
+model.add(Dense(100))
+
 
 # Fully connected 50 neurons
+model.add(Dense(50))
+
 
 # Fully connected 10 neurons (?)
+model.add(Dense(10))
+
 
 # output vehicle control
 model.add(Dense(1))
