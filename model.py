331960bcb1b04ddb1ca186f22659927f2dd65825
Peter Schüllermann <Peter.schuellermann>
2017-05-08 12:00:28 +0200

    clean up old code

--- model.py
+++ model.py
@@ -30,49 +30,7 @@ from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
 
-# import all images from the available data 
-# and append them to arrays for easier use
-#for line in lines:
-#    source_path = line[0]
-#    filename = source_path.split('/')[-1]
-#    center_path = data_path + 'IMG/' + filename
-#    
-#    source_path = line[1]
-#    filename = source_path.split('/')[-1]
-#    left_path = data_path + 'IMG/' + filename
-#
-#    source_path = line[2]
-#    filename = source_path.split('/')[-1]
-#    right_path = data_path + 'IMG/' + filename
-#
-#    image_center = cv2.imread(center_path)
-#    image_left = cv2.imread(left_path)
-#    image_right= cv2.imread(right_path)
-#    measurement_center = float(line[3])
-#    measurement_left = measurement_center + correction
-#    measurement_right = measurement_center - correction
-#    
-#    
-#    images.append(image_center)
-#    measurements.append(measurement_center)
-#    # also append a flipped version of the image
-#    images.append(np.fliplr(image_center))
-#    measurements.append(-measurement_center)
-
-    # append image left
-    #images.append(image_left)
-    #measurements.append(measurement_left)
-    # also append a flipped version of the image
-    #images.append(np.fliplr(image_left))
-    #measurements.append(-measurement_left)
-
-    # append image right
-    #images.append(image_right)
-    #measurements.append(measurement_right)
-    # also append a flipped version of the image
-    #images.append(np.fliplr(image_right))
-    #measurements.append(-measurement_right)
-
+# Generator for memory efficient data loading
 correction = 255.0*0.2
 def generator(samples, batch_size=32):
     num_samples = len(samples)
@@ -105,19 +63,12 @@ def generator(samples, batch_size=32):
 
 
 print("Loaded Data!")
-# convert the image and measurements array
-# to serve as training data
-#X_train = np.array(images)
-#Y_train = np.array(measurements)
-
 
 # todo: split data
 
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Conv2D, Lambda, Cropping2D
-
-ch, row, col = 3, 160, 320  # Trimmed image format
 
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
@@ -136,7 +87,7 @@ model.add(Flatten(input_shape=(160,320,3)))
 model.add(Lambda(lambda x: (x / 255.0) - 0.5))
 
 #model.add(Flatten())
-#model.add(Cropping2D(cropping=((70,25),(0,0))))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
 
 # Convolution 24@31x98
 #model.add(Conv2D(24, 5, 5, subsample=(2,2), activation = "relu"))
